acd3992a8b5c22b323d9479baa2a3c9dc5bc7660
Parsa <if23b139@technikum-wien.at>
2026-03-08 14:34:43 +0100

    project backend structure created

---
+++ tourplanner-backend/src/main/java/com/tourplanner/backend/TourplannerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tourplanner.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourplannerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourplannerBackendApplication.class, args);
+    }
+
+}
